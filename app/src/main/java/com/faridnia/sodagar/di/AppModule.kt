04252a7022e2ad1fc72ff5bab177685f355734c8
Milad Faridnia <miladfaridnia@gmail.com>
2023-07-13 00:09:12 +0330

    Feat: Add GetCoinListUseCase and GetCoinUseCase

---
+++ app/src/main/java/com/faridnia/sodagar/di/AppModule.kt
@@ -0,0 +1,32 @@
+package com.faridnia.sodagar.di
+
+import com.faridnia.sodagar.data.remote.CoinPaprikaApi
+import com.faridnia.sodagar.data.repository.CoinRepositoryImpl
+import com.faridnia.sodagar.domain.repository.CoinRepository
+import com.faridnia.sodagar.util.Constants
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class AppModule {
+
+    @Provides
+    @Singleton
+    fun provideApi(): CoinPaprikaApi {
+        return Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(CoinPaprikaApi::class.java)
+    }
+
+    fun provideRepository(api: CoinPaprikaApi): CoinRepository {
+        return CoinRepositoryImpl(api)
+    }
+}
