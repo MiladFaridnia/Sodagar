2de3225c90bde47db76c3c6aca579a652a76d4ab
Milad Faridnia <miladfaridnia@gmail.com>
2023-07-12 15:59:05 +0330

    Feat: Theme arrangement

--- app/src/main/java/com/faridnia/sodagar/ui/theme/Color.kt
+++ app/src/main/java/com/faridnia/sodagar/ui/theme/Color.kt
@@ -2,10 +2,11 @@ package com.faridnia.sodagar.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
 val Purple40 = Color(0xFF2D224E)
+val Purple80 = Color(0xFFD0BCFF)
+
 val PurpleGrey40 = Color(0xFF8A7CA7)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+
+val Pink80 = Color(0xFFEFB8C8)
 val Pink40 = Color(0xFF7D5260)
