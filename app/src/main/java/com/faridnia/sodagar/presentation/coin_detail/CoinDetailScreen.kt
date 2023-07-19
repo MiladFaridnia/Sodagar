ec41a7d05571a8aef21300100512c10541baae67
Milad Faridnia <miladfaridnia@gmail.com>
2023-07-19 14:06:42 +0330

    Feat: Add coin detail view

---
+++ app/src/main/java/com/faridnia/sodagar/presentation/coin_detail/CoinDetailScreen.kt
@@ -0,0 +1,109 @@
+package com.faridnia.sodagar.presentation.coin_detail
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Divider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterVertically
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.faridnia.sodagar.presentation.coin_detail.components.CoinTag
+import com.faridnia.sodagar.presentation.coin_detail.components.TeamListItem
+import com.google.accompanist.flowlayout.FlowRow
+
+@Composable
+fun CoinDetailScreen(
+    viewModel: CoinDetailViewModel = hiltViewModel()
+) {
+    val state = viewModel.state.value
+    Box(modifier = Modifier.fillMaxSize()) {
+        state.coinDetail.let { coin ->
+            LazyColumn(
+                modifier = Modifier.fillMaxSize(),
+                contentPadding = PaddingValues(20.dp)
+            ) {
+                item {
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        Text(
+                            text = "${coin?.rank}. ${coin?.name} (${coin?.symbol})",
+                            style = MaterialTheme.typography.headlineMedium,
+                            modifier = Modifier.weight(8f)
+                        )
+                        Text(
+                            text = if (coin?.isActive == true) "active" else "inactive",
+                            color = if (coin?.isActive == true) Color.Green else Color.Red,
+                            fontStyle = FontStyle.Italic,
+                            textAlign = TextAlign.End,
+                            modifier = Modifier
+                                .align(CenterVertically)
+                                .weight(2f)
+                        )
+                    }
+                    Spacer(modifier = Modifier.height(15.dp))
+                    Text(
+                        text = coin?.description ?: "",
+                        style = MaterialTheme.typography.bodyMedium
+                    )
+                    Spacer(modifier = Modifier.height(15.dp))
+                    Text(
+                        text = "Tags",
+                        style = MaterialTheme.typography.headlineSmall
+                    )
+                    Spacer(modifier = Modifier.height(15.dp))
+                    FlowRow(
+                        mainAxisSpacing = 10.dp,
+                        crossAxisSpacing = 10.dp,
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        coin?.tags?.forEach { tag ->
+                            CoinTag(tag = tag)
+                        }
+                    }
+                    Spacer(modifier = Modifier.height(15.dp))
+                    Text(
+                        text = "Team members",
+                        style = MaterialTheme.typography.headlineSmall
+                    )
+                    Spacer(modifier = Modifier.height(15.dp))
+                }
+                coin?.team?.let {
+                    items(it) { teamMember ->
+                        TeamListItem(
+                            teamMember = teamMember,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(10.dp)
+                        )
+                        Divider()
+                    }
+                }
+            }
+        }
+        if (state.error.isNotBlank()) {
+            Text(
+                text = state.error,
+                color = MaterialTheme.colorScheme.error,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 20.dp)
+                    .align(Alignment.Center)
+            )
+        }
+        if (state.isLoading) {
+            CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+        }
+    }
+}
